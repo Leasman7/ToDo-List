8a1b133fa8903d95cc94269e07c4271e3941768e
Savannah Leasman <leasman7@gmail.com>
2021-08-31 18:12:43 -0500

    can now add and remove items in todo

--- index.js
+++ index.js
@@ -13,22 +13,40 @@ function App() {
       isCompleted: false,
     },
   ]);
-  const [value, setValue] = React.useState(''); //add new managed variable
-  
+  const [value, setValue] = React.useState(""); //add new managed variable
+
   //handling what happens when user hits submit
-  const handleSubmit = e => {
-      e.preventDefault();
-      if (!value) return; //check for existing value
-      const newTodos = [...todos, {text: value, isCompleted: false}]; //construct new list -existing list plus new todo
-      setTodos(newTodos); //add input
-      setValue(''); //clear form
-  }
-  return (<>
-    {todos.map((todo, i) => <div className="todo" key={i}>{todo.text}</div>)}
-    <form onSubmit = {handleSubmit}>
-        <input type="text" className="input" value={value} placeholder="Add ToDo ..." onChange={e => setValue(e.target.value)}/>
-    </form>
-  </>);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return; //check for existing value
+    const newTodos = [...todos, { text: value, isCompleted: false }]; //construct new list -existing list plus new todo
+    setTodos(newTodos); //add input
+    setValue(""); //clear form
+  };
+  const removeTodo = (e) => {
+    const index = Number(e.target.id);
+    let temp = [...todos];
+    temp.splice(index, 1);
+    setTodos(temp);
+  };
+  return (
+    <>
+      {todos.map((todo, i) => (
+        <div className="todo" key={i} id={i} onClick={removeTodo}>
+          {todo.text}
+        </div>
+      ))}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="input"
+          value={value}
+          placeholder="Add ToDo ..."
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </form>
+    </>
+  );
 }
 
-ReactDOM.render(<App/>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
